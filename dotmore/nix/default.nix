00e3e034ae14e6485072399b300e0079952e9fb1
Caspar Krieger <git@asparck.com>
2020-07-07 21:05:19 -0700

    Add more packages

--- dotmore/nix/default.nix
+++ dotmore/nix/default.nix
@@ -9,18 +9,24 @@ in pkgs // {
   ckrieger-devtools = pkgs.buildEnv {
     name = "ckrieger-devtools";
     paths = with pkgs; [
+      cvs
+      clang
       cmake
       emacsUnstable
       entr
+      fd
       fish
       fzf
+      git
       htop
       jq
       lua
       neovim
       niv
       nixfmt
+      mercurialFull
       pythonFull
+      subversion
       ripgrep
       ruby
       rustup
