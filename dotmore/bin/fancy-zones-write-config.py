805cb13c59eaaf95bd97efa27e0be7a2b723f0cf
Caspar Krieger <git@asparck.com>
2022-02-02 16:44:56 +0800

    fancyzones - add left-biased layout

--- dotmore/bin/fancy-zones-write-config.py
+++ dotmore/bin/fancy-zones-write-config.py
@@ -10,12 +10,16 @@ fancy_zones_config_file = "/mnt/c/Users/Admin/AppData/Local/Microsoft/PowerToys/
 screen_width = 3072
 screen_height = 1240
 
+col = 3072 // 12
+
+left_tiny = 400
 left_small = 850
 left_med = 1100
 left_big = 1400
 
-middle = screen_width / 2
+middle = screen_width // 2
 
+right_tiny = screen_width - left_tiny
 right_small = screen_width - left_small
 right_med = screen_width - left_med
 right_big = screen_width - left_big
@@ -34,21 +38,6 @@ def xx(x1, x2):
     return xw(x1, x2 - x1)
 
 
-layout = [
-    # xx(0, middle),
-    xx(0, left_small),
-    xx(0, left_med),
-    xx(0, left_big),
-    xx(left_small, right_med),  # off center to left
-    xx(left_med, right_med),  # narrow in center
-    xx(left_small, right_small),  # big in center
-    xx(left_med, right_small),  # off center to right
-    xx(right_big, screen_width),
-    xx(right_med, screen_width),
-    xx(right_small, screen_width),
-    # xx(middle, screen_width),
-]
-
 import json
 
 with open(fancy_zones_config_file, "r") as f:
@@ -62,7 +51,38 @@ existing["custom-zone-sets"] = [
         "info": {
             "ref-width": screen_width,
             "ref-height": screen_height,
-            "zones": layout,
+            "zones": [
+                # xx(0, middle),
+                xx(0, left_small),
+                xx(0, left_med),
+                xx(0, left_big),
+                xx(left_small, right_med),  # off center to left
+                xx(left_med, right_med),  # narrow in center
+                xx(left_small, right_small),  # big in center
+                xx(left_med, right_small),  # off center to right
+                xx(right_big, screen_width),
+                xx(right_med, screen_width),
+                xx(right_small, screen_width),
+                # xx(middle, screen_width),
+            ],
+        },
+    },
+    {
+        "uuid": "{B463AC29-54DF-4933-A190-072332A04FFB}",
+        "name": "Left-biased",
+        "type": "canvas",
+        "info": {
+            "ref-width": screen_width,
+            "ref-height": screen_height,
+            "zones": [
+                xx(col*0, col*4),
+                xx(col*0, col*6),
+                xx(col*1, col*7),
+                xx(col*2, col*7),
+                xx(col*4, col*8),
+                xx(col*6, col*12),
+                xx(col*8, col*12),
+            ],
         },
     },
     # hand-built layouts from here on down
