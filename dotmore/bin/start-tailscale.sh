15d735d2fecb8fd992dad1d23ec6a0f78c9c2d4a
Caspar Krieger <git@asparck.com>
2023-05-02 10:02:34 +0800

    Add script to start tailscale

---
+++ dotmore/bin/start-tailscale.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Starting Tailscale daemon automatically if not running...
+RUNNING=`ps aux | grep tailscaled | grep -v grep`
+if [ -z "$RUNNING" ]; then
+    sudo tailscaled > /dev/null 2>&1 &
+    disown
+fi
